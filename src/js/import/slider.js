5419312a25b49bc65a4143b5ba4238cb93458d29
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-06-20 17:24:34 +0300

    fix

--- src/js/import/slider.js
+++ src/js/import/slider.js
@@ -12,3 +12,40 @@ var swiper = new Swiper('.swiper-intro', {
     clickable: true,
   },
 });
+
+//special-offers
+
+var swiper = new Swiper('.swiper-special-offers', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-special-offers-next',
+    prevEl: '.swiper-special-offers-prev',
+  },
+});
+
+
+//gallery
+
+var swiper = new Swiper('.swiper-gallery', {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-gallery-next',
+    prevEl: '.swiper-gallery-prev',
+  },
+  pagination: {
+    el: '.swiper-gallery-pagination',
+    clickable: true,
+  },
+});
+
+
+//reviews
+
+var swiper = new Swiper('.swiper-reviews', {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-reviews-next',
+    prevEl: '.swiper-reviews-prev',
+  },
+});
--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -1,4 +1,6 @@
 import 'jquery-modal';
+import range from 'rangeslider.js';
+import '../lib/maskedinput.js';
 
 //header
 $(window).scroll(function() {
@@ -8,3 +10,31 @@ $(window).scroll(function() {
     $('.header__scroll').removeClass('active');
   }
 });
+
+//select
+$('select').each(function() {
+  $(this).siblings('p').text( $(this).children('option:selected').text() );
+});
+$('select').change(function() {
+  $(this).siblings('p').text( $(this).children('option:selected').text() );
+});
+
+
+//range
+
+$('input[type="range"]').rangeslider({
+  polyfill: false,
+  onSlide: function() {
+    day();
+  },
+});
+
+function day() {
+  let text = $('.calculator .form__range p');
+  let valinput = $('.calculator .form__range input[type="range"]').val();
+  $(text).text(valinput + ' дней');
+}
+
+
+//mask input
+$('.phone').mask('+7 (9 9 9) 9 9 9 - 9 9 - 9 9');
